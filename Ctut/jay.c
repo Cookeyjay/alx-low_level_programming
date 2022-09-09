572daf1aea0fd0fe012e6a783ae3de894e3c98bd
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-09 11:56:12 -0700

    My ever first c program

---
+++ Ctut/jay.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*tutorials*/
+int main(void)
+{
+	char name1[40];
+	printf("Please input your name:   \n");
+	scanf("%s\n", name1);
+	printf("Your name is %s\n", name1);
+	return (0);
+}
