62769459619d3b285fc657fa5d9135312b2e24ce
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-10 05:32:54 -0700

    +ve && -ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,8 @@
 /**
  * main - Prints random positive or negative numbers
  *
- * Description: This program prints random numbers using the main function and declare uf the numbers printed are negative or positive
+ * Description: This program prints random numbers using the main function
+ *
  * Return: 0
  */
 int main(void)
@@ -12,12 +13,17 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) {
-		printf("%d is positive.", n);
-	} else if(n == 0) {
-		printf("%d is zero.", n);
-	} else {
-		printf("%d is negative.", n);
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
 	};
 	return (0);
 }
