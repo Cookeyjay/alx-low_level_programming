076ab9509f436303918960101bffea00d3b6eccb
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-10 17:04:29 -0700

    Base 16 numvers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlin.h>
+/**
+ * main - Prints base 16 numbers
+ *
+ * Description: Use the main function to print base 16 numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	char c;
+
+	for (x = 0; x <= 10; x++)
+	{
+		putchar((x % 10) + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
