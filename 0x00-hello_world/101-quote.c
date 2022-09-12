7644c41c6e603b97f5920d2fd2af5b2b841a656a
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-12 05:35:57 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints a string
+ *
+ * Description: Prints string using the main function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
