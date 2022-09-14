48aea8ef883171fb36fcbe941db0b91ca25cff91
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-13 19:24:05 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,13 @@
-int _putchar(int ch)
-{
-	return (0);
-}
+#ifndef main_h
+#define main_h
+
+/**
+ * File_name - main.h
+ *
+ * Description: declaration of functions used in 
+ * 0x02-functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+#endif
