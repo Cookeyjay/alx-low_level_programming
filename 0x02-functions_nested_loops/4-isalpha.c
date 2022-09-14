ddb9e70f9e6726292a887e00d080c4ff08f28899
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-13 21:53:04 -0700

    alphaNum

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ *
+ * @c: The character to be checked.
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
