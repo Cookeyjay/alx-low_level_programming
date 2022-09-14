aee0981bba1f65c63d9993621ed2a3e6336278af
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-13 20:55:48 -0700

    alpha x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * void print_alphabet_x10 - Prints 10 times the alphabets
+ * print_alphabet_x10 - Prints 10 times the alphabets
  *
  * Description: Use print_alphabet_x10 to prints
  * the alphabets in lowercase 10 times plus a new line
