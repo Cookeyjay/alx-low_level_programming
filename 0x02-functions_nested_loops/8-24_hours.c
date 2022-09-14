2f204b37a06c565ece8804566c744a852b6e82a0
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-14 11:32:51 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,14 @@ void jack_bauer(void)
 
 	for (n = 0; n <= 23; n++)
 	{
-		for (m = 0; <= 59; m++)
+		for (m = 0; m <= 59; m++)
 		{
 			_putchar(n / 10 + '0');
 			_putchar(n % 10 + '0');
 			_putchar(':');
 			_putchar(m / 10 + '0');
 			_putchar(m % 10 + '0');
-			_putchat('\n');
+			_putchar('\n');
 		}
 	}
 }
