adb6a5c94c755aeb66cc3493722eb36606f6a1b8
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-13 20:21:40 -0700

    lowercase alphabets corrected

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * print_alphabets - prints lowercase  alphabets
+ *
+ * Description: use the print_alphabets function to print lowercase alpha
  * Return: 0
  */
 void print_alphabet(void)
