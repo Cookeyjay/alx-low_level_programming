d49268111f9b6186fe68ca139c19b2e50d86ccd5
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-15 07:08:48 -0700

    9x tanle

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int n;
+	int j;
+
+	prod;
+
+	prod = n * j;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (prod >= 10)
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			else
+			{
+				_putchar((prod % 10) + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
