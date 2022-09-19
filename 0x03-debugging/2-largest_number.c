15150140dd47be14a7802ba8922a2fbd4a5dfa09
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-09-19 11:27:31 -0700

    multiple mains

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,11 +9,13 @@
  */
 int largest_number(int a, int b, int c)
 {
-	if ((a > b && b > c) || (a > c && c > b))
+	largest;
+
+	if (a > b && b > c || a > c && c > b)
 	{
 		largest = a;
 	}
-	else if ((b > a && a > c) || (b > c && c > a))
+	else if (b > a && a > c || b > c && c > a)
 	{
 		largest = b;
 	}
