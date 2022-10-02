7ecf73c739342c8d9d2cf44c54a592c72509fdfa
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-10-01 17:26:41 -0700

    Number of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: counts arguement
+ * @argv: arguements
+ *
+ * Return: 0
+ */
+int main(int argc, *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
