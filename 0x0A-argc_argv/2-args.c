aff9c4703f19335b2ea895d99aa40a7c419122de
CookeyGam_Jerry <cookeygamjerry@gmail.com>
2022-10-01 18:02:36 -0700

    all arguements

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,15 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int count;
-
-	if (argc > 0)
-	{
-		for (count = 0; count < argc; count++)
-		{
-			printf("%d\n", argv[count]);
-		}
-	}
+	(void)argv;
+	printf("%d\n", argc);
 
 	return (0);
 }
